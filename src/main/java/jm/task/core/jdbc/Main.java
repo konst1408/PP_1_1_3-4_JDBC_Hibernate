f9e15addc8857ff6c636003e04d00ecd59cf3659
konst1408 <kostya.kormashov@gmail.com>
2023-01-22 16:31:13 +0300

    Worked BD

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,19 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userServiceImpl = new UserServiceImpl();
-        userServiceImpl.createUsersTable();
-        userServiceImpl.saveUser("Kostya", "Kormashov", (byte) 28);
-        userServiceImpl.saveUser("Maks", "Dugaev", (byte) 28);
-        userServiceImpl.saveUser("Elon", "Mask", (byte) 51);
-        userServiceImpl.saveUser("Alexander", "Sokolovsky", (byte) 28);
-        userServiceImpl.getAllUsers();
-        userServiceImpl.cleanUsersTable();
-        userServiceImpl.dropUsersTable();
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Kostya", "Kormashov", (byte) 28);
+        userDaoJDBC.saveUser("Maks", "Dugaev", (byte) 28);
+        userDaoJDBC.saveUser("Elon", "Mask", (byte) 51);
+        userDaoJDBC.saveUser("Alexander", "Sokolovsky", (byte) 28);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
 
     }
 }
